31ba9c6abed1d7b1cad4114e2899c8ac2da1be14
SBSun <byung0216@gmail.com>
2025-12-30 13:43:15 +0900

    feat: Setup localflow-server project

---
+++ src/main/java/com/localflow/LocalFlowServerApplication.java
@@ -0,0 +1,13 @@
+package com.localflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalFlowServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalFlowServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/localflow/LocalFlowServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalFlowServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
